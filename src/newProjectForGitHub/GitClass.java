ba48459f81702950be676910ca3257a141f50cd4
Zikria Misbah <zikriya.af@gmail.com>
2022-05-22 18:52:43 -0400

    multi D Array

---
+++ src/newProjectForGitHub/GitClass.java
@@ -0,0 +1,20 @@
+package newProjectForGitHub;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		String [][] cars = {{"Mercedes","VW"},{"Honda","Acura"},{"Toyota","Lexus"}};
+		String x = cars[2][1];
+		System.out.println(x);
+		System.out.println("================");
+		
+		for(int i =0; i<cars.length; ++i) {
+			for (int j = 0; j<cars[i].length;++j) {
+				System.out.println(cars[i][j]);
+			}
+			
+		}
+
+	}
+
+}
