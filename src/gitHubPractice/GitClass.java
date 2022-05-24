33acbefb5b8107092e83cf27eed827f14fdb0216
Zikria Misbah <zikriya.af@gmail.com>
2022-05-24 18:50:43 -0400

    see the changes

--- src/gitHubPractice/GitClass.java
+++ src/gitHubPractice/GitClass.java
@@ -3,9 +3,9 @@ package gitHubPractice;
 public class GitClass {
 
 	public static void main(String[] args) {
-		String [][] cars = {{"Mercedes","VW"},{"Honda","Acura"},{"Toyota","Lexus"}};
-		//String x = cars[2][1];
-		//System.out.println(x);
+		String[][] cars = { { "Mercedes", "VW" }, { "Honda", "Acura" }, { "Toyota", "Lexus" } };
+		// String x = cars[2][1];
+		// System.out.println(x);
 		System.out.println(cars[0][0]);
 		System.out.println(cars[0][1]);
 		System.out.println(cars[1][0]);
@@ -13,12 +13,12 @@ public class GitClass {
 		System.out.println(cars[2][0]);
 		System.out.println(cars[2][1]);
 		System.out.println("================");
-		
-		for(int i =0; i<cars.length; ++i) {
-			for (int j = 0; j<cars[i].length;++j) {
+
+		for (int i = 0; i < cars.length; ++i) {
+			for (int j = 0; j < cars[i].length; ++j) {
 				System.out.println(cars[i][j]);
 			}
-			
+
 		}
 
 	}
