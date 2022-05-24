a60353b227f17db34423ba2eca49ca618b289a45
Zikria Misbah <zikriya.af@gmail.com>
2022-05-24 16:17:37 -0400

    2D Array Changes

--- src/gitHubPractice/GitClass.java
+++ src/gitHubPractice/GitClass.java
@@ -4,8 +4,14 @@ public class GitClass {
 
 	public static void main(String[] args) {
 		String [][] cars = {{"Mercedes","VW"},{"Honda","Acura"},{"Toyota","Lexus"}};
-		String x = cars[2][1];
-		System.out.println(x);
+		//String x = cars[2][1];
+		//System.out.println(x);
+		System.out.println(cars[0][0]);
+		System.out.println(cars[0][1]);
+		System.out.println(cars[1][0]);
+		System.out.println(cars[1][1]);
+		System.out.println(cars[2][0]);
+		System.out.println(cars[2][1]);
 		System.out.println("================");
 		
 		for(int i =0; i<cars.length; ++i) {
